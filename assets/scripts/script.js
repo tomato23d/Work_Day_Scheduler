441db5726ddddef0f10a403020f0662830a175eb
tomato23d <anna.a.art@gmail.com>
2023-03-14 14:13:39 +1100

    updates to functions

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,9 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 $(document).ready(function(){});
 
-const workhours = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm"] 
+const workhours = ["7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm",  "5pm", "6pm"] 
+
+
 
 var day = $(".container_day1");
 //var hour =document.getElementById("hour"); 
@@ -15,21 +15,13 @@ var hourFuture =$(".future");
 var btnNextHour = $("#nextHour");
 var currentTime = dayjs().format('ha');
 //var currentTime = "12pm";
-//var dayy = $("#currentDay")+1;
-console.log(hour);
+
+
+ //var dayy = dayjs().hour(5);
+
+
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
- 
+
 });
 
 $("#currentDay").text(dayjs().format('[Today is] D MMMM YYYY [,] dddd'));
@@ -38,13 +30,15 @@ console.log(currentTime);
 
 
 function populateTask (){
-  for (let i=0; i<workhours.length; i++){
-      $("#entryHour").clone().appendTo(hour);
-      $("#task").clone().appendTo(hour);
-      $("#saveBtn").clone().appendTo(hour);
-      $("#entryHour").text(workhours[i]);  
-      hour.appendTo(day);   
-  } ;  
+ for (let i=0; i<workhours.length; i++){
+     $("#entryHour").clone().appendTo(hour);
+     $("#task").clone().appendTo(hour);
+     $("#saveBtn").clone().appendTo(hour);
+     $("#entryHour").text(workhours[i]);  
+     $("#hour").clone().appendTo(day);
+     
+    
+ } ;  
 };
   function reviewTime(){
      for (let h=0; h<workhours.length; h++){
@@ -52,14 +46,17 @@ function populateTask (){
               hour.addClass('future');
               console.log("future: " + workhours[h]);
               populateTask();
+              
                   }
             else if (workhours[h]<currentTime){
               hour.addClass('past');
               console.log("past : " + workhours[h]);
+              populateTask();
                   }
             else  {
               hour.addClass('present');
               console.log("present : " + workhours[h]);
+              populateTask();
                   }
           };}
           
